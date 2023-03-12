9a80434af3f82146c44278cfecee205632a43cc2
Gary <garyarneys@gmail.com>
2023-03-12 05:52:52 -0500

    Completed POST on part 3

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+   const { title, content, tags = "" } = req.body;
+
+   const tagArr = tags.trim().split(/\s+/)
+   const postData = {};
+
+   if (tagArr.length) {
+    postData.tags = tagArr;
+   }
+
+   try {
+//add
+    postData.authorId = req.user.id
+    postData.title = title
+    postData.content = content
+    console.log('content', postData.content);
+    const post = await createPost(postData);
+    if (post) {
+        res.send({ post });
+    } else {
+        next({
+            name: 'PostERROR',
+            message: 'Nothing posted.'
+          });
+    }
+   } catch ({ name, message }) {
+    next({ name, message });
+   }
+});
 
 postsRouter.use((req, res, next) => {
     console.log("Request made to posts");
