355e41e2b427cf62f1f1e783b699b213b78c6996
Gary <garyarneys@gmail.com>
2023-03-12 15:25:17 -0500

    Finished PATCH section of part 3

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts, createPost } = require('../db');
+const { getAllPosts, createPost, updatePost, getPostById } = require('../db');
 const { requireUser } = require('./utils');
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
@@ -12,13 +12,10 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
    if (tagArr.length) {
     postData.tags = tagArr;
    }
-
    try {
-//add
     postData.authorId = req.user.id
     postData.title = title
     postData.content = content
-    console.log('content', postData.content);
     const post = await createPost(postData);
     if (post) {
         res.send({ post });
@@ -33,6 +30,8 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
    }
 });
 
+
+
 postsRouter.use((req, res, next) => {
     console.log("Request made to posts");
 
@@ -46,5 +45,43 @@ postsRouter.get('/', async (req, res) => {
         posts
     });
 });
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
+
+    const updateFields = {};
+
+    if (tags && tags.length > 0) {
+        updateFields.tags = tags.trim().split(/\s+/);
+    }
+
+    if (title) {
+        updateFields.title = title;
+    }
+
+    if (content) {
+        updateFields.content = content;
+    }
+
+    try {
+        const originalPost = await getPostById(postId);
+        if (originalPost.author.id === req.user.id) {
+            const updatedPost = await updatePost(postId, updateFields);
+            res.send({ post: updatedPost })
+        } else {
+            next({
+                name: 'UnauthorizedUserError',
+                message: 'You cannot update a post that is not yours'
+            })
+        }
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
+
+   
+
 
 module.exports = postsRouter;
